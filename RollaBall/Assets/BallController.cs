035bb96a50ef7c00cf977d25bc0f6c6df73406d6
KaHEE Kang <77781435+Cecilia7768@users.noreply.github.com>
2021-04-09 08:31:55 +0900

    RollerBall

---
+++ RollaBall/Assets/BallController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody body;
+    int score;
+    public float ballSpeed;
+    public Text CountText;
+    public Text WinText;
+    public float timelimit ;
+    public Text timer;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        score = 0;
+        setCountText();
+    }
+    private void Awake()
+    {
+        timelimit = 30f;
+    }
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+
+        if (timelimit > 0 && score < 3)
+        {
+            timelimit -= Time.deltaTime;
+        }
+        else if (timelimit <= 0)
+        {
+            WinText.text = "You Lose ! ";
+            Application.Quit();
+        }
+
+        timer.text = string.Format("{0:N2}", timelimit);     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+            setCountText();
+        }
+    }
+    void setCountText()
+    {
+        WinText.text = "";
+        CountText.text = "Score : " + score.ToString();
+        if (score >= 3)
+        {
+            WinText.text = "You Win ! ";
+            Application.Quit();
+        }
+    }
+
+}
+
---
+++ RollaBall/Assets/CameraMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    //3인칭으로 게임오브젝트를 보기위해 설정
+    //게임오브젝트에서 멀리 떨어져있어야하므로 퍼블릭으로 이동
+
+    public GameObject target;
+    public float xOffset, yOffset, zoffset;
+    void Update()
+    {
+        //위치를 이동하고 조금 멀어지게해야 하므로 new vec 좌표설정
+        transform.position = target.transform.position + new Vector3(xOffset,yOffset,zoffset);
+
+        transform.LookAt(target.transform.position);
+    }
+}
