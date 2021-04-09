f35e73084c75ff5da2ccebe08d556171c9371c80
KaHEE Kang <77781435+Cecilia7768@users.noreply.github.com>
2021-04-09 10:10:17 +0900

    BamsongEee

---
+++ BamsongEee/Assets/BamsongiGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    public GameObject bamsongiPrefab; 
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bamsongi = Instantiate(bamsongiPrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldir = ray.direction;
+            bamsongi.GetComponent<BamsongiController>().Shoot(worldir.normalized*2000);
+        }
+    }
+}
