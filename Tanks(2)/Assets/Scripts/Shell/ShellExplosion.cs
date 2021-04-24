b301a6c6ca5a5d7a21c408dd34a702fc32573035
Cecilia7768 <77781435+Cecilia7768@users.noreply.github.com>
2021-04-24 16:55:32 +0900

    4.24

---
+++ Tanks(2)/Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShellExplosion : MonoBehaviour
+{
+	public LayerMask m_TankMask;
+	public ParticleSystem m_ExplosionParticles;
+	public AudioSource m_ExplosionAudio;
+	public float m_MaxDamage = 100f;
+	public float m_ExplosionForce = 1000f;
+	public float m_MaxLifeTime = 2f;
+	public float m_ExplosionRadius = 5f;
+	//미사일 데미지,발사속도 등등
+	//public TankShooting shooter;
+    private float m_nomalDamage;
+    public float m_PowerUpDamage;
+    public bool m_ChangedPower;
+
+
+	private void Start()
+	{
+		Destroy(gameObject, m_MaxLifeTime);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Find all the tanks in an area around the shell and damage them.
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			Rigidbody targetRigidBody = colliders[i].GetComponent<Rigidbody>();
+
+			if (!targetRigidBody)
+			{
+				continue;
+			}
+
+			targetRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+			TankHealth targetHealth = targetRigidBody.GetComponent<TankHealth>();
+
+			if (!targetHealth)
+			{
+				continue;
+			}
+			float damage;
+			
+			damage = CalculateDamage(targetRigidBody.position);
+			targetHealth.TakeDamage(damage);
+		}
+
+		m_ExplosionParticles.transform.parent = null;
+		m_ExplosionParticles.Play();
+		m_ExplosionAudio.Play();
+
+		Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+		Destroy(gameObject);
+	}
+
+	private float CalculateDamage(Vector3 targetPosition)
+	{
+		Vector3 explosionToTarget = targetPosition - transform.position;
+		float explosionDistance = explosionToTarget.magnitude;
+		float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+		float damage;
+		if (m_PowerUpDamage > 0)
+		{
+			damage = m_PowerUpDamage + (relativeDistance * m_MaxDamage);
+			m_ChangedPower = true;
+		}
+		else
+			damage = relativeDistance * m_MaxDamage; 
+
+		damage = Mathf.Max(0f, damage);
+
+		return damage;
+	}
+
+	IEnumerator Delay_Time(Rigidbody targetRigidBody)
+	{
+		float damage;
+		yield return new WaitForSeconds(4f);
+		damage = CalculateDamage(targetRigidBody.position);
+
+	}
+}
+
---
+++ Tanks(2)/Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+public class TankHealth : MonoBehaviour
+{
+    public float m_StartingHealth = 100f;
+    public Slider m_Slider;
+    public Image m_FillImage;
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
+    public GameObject m_ExplosionPrefab;
+
+    private AudioSource m_ExplosionAudio;
+    private ParticleSystem m_ExplosionParticles;
+    public float m_CurrentHealth;
+    private bool m_Dead;
+    //////////////////////////    
+    public float m_addLife = 20f;
+    public bool m_PowerUpCoin;
+    public bool m_SpeedCoin;
+
+    //스테이지 새로시작시 코인,파워 초기화
+
+    private void Awake()
+    {
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+
+        m_ExplosionParticles.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        int randomEffect;
+        randomEffect = Random.Range(0, 3);
+        if (collider.gameObject.CompareTag("Coin"))
+        {
+            collider.gameObject.SetActive(false);
+            StartCoroutine(WhenMeetCoin(1));
+        }
+        SetHealthUI();
+    }
+
+    IEnumerator WhenMeetCoin(int randomEffect)
+    {
+
+        if (randomEffect == 0)
+        {
+            if (m_CurrentHealth + m_addLife <= 100)
+                m_CurrentHealth += m_addLife;
+            else
+                m_CurrentHealth = m_StartingHealth;
+            yield break;
+        }
+        else if (randomEffect == 1)
+        {               
+            m_PowerUpCoin = true;
+            yield return new WaitForSeconds(4f);
+            m_PowerUpCoin = false;
+
+        }
+        else if (randomEffect == 2)     
+        {
+            m_SpeedCoin = true;
+            yield break;
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+
+        SetHealthUI();
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        m_CurrentHealth -= amount;
+        SetHealthUI();
+
+        if (m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
+    }
+
+
+    private void SetHealthUI()
+    {
+        // Adjust the value and colour of the slider.
+        m_Slider.value = m_CurrentHealth;
+
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
+
+
+    private void OnDeath()
+    {
+        // Play the effects for the death of the tank and deactivate it.
+        m_Dead = true;
+
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        gameObject.SetActive(false);
+    }
+}
+
